ae4dd30c39b0e46120ac5a055eb325885c7fd58f
Laszlo Lueck <llueck@gmail.com>
2021-07-01 21:38:16 +0200

    update

--- DocSearchAIO/Classes/ReverseComparerService.cs
+++ DocSearchAIO/Classes/ReverseComparerService.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Akka;
 using Akka.Actor;
 using Akka.Streams;
 using Akka.Streams.Dsl;
@@ -20,36 +21,43 @@ namespace DocSearchAIO.Classes
 {
     internal static class ReverseComparerServiceHelper
     {
-        public static async Task<Maybe<ComparerObject>> CheckCacheEntry(this ComparerObject compObj, InterlockedCounter allFileCount)
+        public static Source<Maybe<ComparerObject>, NotUsed> CheckCacheEntry(
+            this Source<ComparerObject, NotUsed> source,
+            InterlockedCounter allFileCount)
         {
-            return await Task.Run(() =>
+            return source.Select(compObj =>
             {
                 allFileCount.Increment();
-                return File.Exists(compObj.OriginalPath) ? Maybe<ComparerObject>.None : Maybe<ComparerObject>.From(compObj);
-
-                //if (File.Exists(compObj.OriginalPath)) return false;
-                //removedFileCount.Increment();
-                //logger.LogInformation("remove obsolete file <{ObsoleteFile}>", compObj.OriginalPath);
-                //cache.TryRemove(new KeyValuePair<string, ComparerObject>(compObj.PathHash, compObj));
-                //return await elasticSearchService.RemoveItemById(indexName, compObj.PathHash);
+                return File.Exists(compObj.OriginalPath)
+                    ? Maybe<ComparerObject>.None
+                    : Maybe<ComparerObject>.From(compObj);
             });
+
+            //if (File.Exists(compObj.OriginalPath)) return false;
+            //removedFileCount.Increment();
+            //logger.LogInformation("remove obsolete file <{ObsoleteFile}>", compObj.OriginalPath);
+            //cache.TryRemove(new KeyValuePair<string, ComparerObject>(compObj.PathHash, compObj));
+            //return await elasticSearchService.RemoveItemById(indexName, compObj.PathHash);
         }
 
-        public static async Task<IEnumerable<ComparerObject>> RemoveFromIndexById(this IEnumerable<ComparerObject> group, InterlockedCounter removedFileCount,
+        public static Source<IEnumerable<ComparerObject>, NotUsed> RemoveFromIndexById(
+            this Source<IEnumerable<ComparerObject>, NotUsed> source, InterlockedCounter removedFileCount,
             IElasticSearchService elasticSearchService, ILogger logger, string indexName)
         {
-            var p = group.ToArray();
-            var resultCount = await elasticSearchService.RemoveItemsById(indexName, p.Select(item => item.PathHash));
-            removedFileCount.Add(resultCount);
-            return p;
+            return source.SelectAsyncUnordered(10, async items =>
+            {
+                var asArray = items.ToArray();
+                var resultCount =
+                    await elasticSearchService.RemoveItemsById(indexName, asArray.Select(item => item.PathHash));
+                removedFileCount.Add(resultCount);
+                return asArray.Select(p => p);
+            });
         }
-
-
     }
 
-    
+
     //, InterlockedCounter removedFileCount, ILogger logger, IElasticSearchService elasticSearchService,ConcurrentDictionary<string, ComparerObject> cache, string indexName
-    
+
     public class ReverseComparerService<T> where T : ComparerModel
     {
         private readonly ILogger _logger;
@@ -69,7 +77,9 @@ namespace DocSearchAIO.Classes
             _actorSystem = actorSystem;
             _allFileCount = new InterlockedCounter();
             _removedFileCount = new InterlockedCounter();
-            _lazyCache = new Lazy<ConcurrentDictionary<string, ComparerObject>>(() => ComparerHelper.FillConcurrentDictionary(_comparerFile));
+            _lazyCache =
+                new Lazy<ConcurrentDictionary<string, ComparerObject>>(() =>
+                    ComparerHelper.FillConcurrentDictionary(_comparerFile));
         }
 
         public async Task Process(string indexName)
@@ -81,30 +91,28 @@ namespace DocSearchAIO.Classes
                 var sw = Stopwatch.StartNew();
                 try
                 {
-                    var cache = ComparerHelper.FillConcurrentDictionary(_comparerFile);
-                    await ComparerHelper
-                        .GetComparerObjectSource(_comparerFile)
-                        .SelectAsyncUnordered(10, async compObj =>
-                            await compObj
-                                .CheckCacheEntry(_allFileCount))
-                        .GroupedWithin(1000, TimeSpan.FromSeconds(10))
-                        .Select(group => group.Values())
-                        .Select(values =>
-                        {
-                            
-                        });
-                        .RunWith(Sink.Ignore<bool>(), _actorSystem.Materializer());
-                    if (_removedFileCount.GetCurrent() > 0)
-                    {
-                        _logger.LogInformation("switch comparer file, there are elements removed");
-                        ComparerHelper.RemoveComparerFile(_comparerFile);
-                        ComparerHelper.CreateComparerFile(_comparerFile);
-                        await ComparerHelper.WriteAllLinesAsync(cache, _comparerFile);
-                    }
-                    else
-                    {
-                        _logger.LogInformation("nothing to do, no elements found which are removed while cleanup");
-                    }
+                    
+                    //var cache = ComparerHelper.FillConcurrentDictionary(_comparerFile);
+                    // await ComparerHelper
+                    //         .GetComparerObjectSource(_comparerFile)
+                    //         .SelectAsyncUnordered(10, async compObj =>
+                    //             await compObj
+                    //                 .CheckCacheEntry(_allFileCount))
+                    //         .GroupedWithin(1000, TimeSpan.FromSeconds(10))
+                    //         .Select(group => group.Values())
+                    //         .Select(values => { });
+                    //     .RunWith(Sink.Ignore<bool>(), _actorSystem.Materializer());
+                    // if (_removedFileCount.GetCurrent() > 0)
+                    // {
+                    //     _logger.LogInformation("switch comparer file, there are elements removed");
+                    //     ComparerHelper.RemoveComparerFile(_comparerFile);
+                    //     ComparerHelper.CreateComparerFile(_comparerFile);
+                    //     await ComparerHelper.WriteAllLinesAsync(cache, _comparerFile);
+                    // }
+                    // else
+                    // {
+                    //     _logger.LogInformation("nothing to do, no elements found which are removed while cleanup");
+                    // }
                 }
                 finally
                 {
