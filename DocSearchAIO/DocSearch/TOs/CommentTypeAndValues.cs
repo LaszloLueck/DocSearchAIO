52585dc3098be264f644af9ac871670c7cf1ba7b
Laszlo Lueck <llueck@gmail.com>
2021-08-22 14:10:29 +0200

    remove unused files

--- DocSearchAIO/DocSearch/TOs/CommentTypeAndValues.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-
-namespace DocSearchAIO.DocSearch.TOs
-{
-    public record CommentTypeAndValues(IEnumerable<CommentDetail> CommentValues);
-}
--- DocSearchAIO/DocSearch/TOs/ContentTypeAndValues.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-namespace DocSearchAIO.DocSearch.TOs
-{
-
-    public record ContentTypeAndValues(IEnumerable<ContentDetail> ContentValues);
-}
