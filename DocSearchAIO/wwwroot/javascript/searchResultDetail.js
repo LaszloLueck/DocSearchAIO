8c375b3dfbed99b3739cbf0d321f063f4929d760
Laszlo Lueck <llueck@gmail.com>
2021-08-23 13:20:11 +0200

    fix comments in search result

--- DocSearchAIO/wwwroot/javascript/searchResultDetail.js
+++ DocSearchAIO/wwwroot/javascript/searchResultDetail.js
@@ -41,7 +41,11 @@ renderSearchResultDetails = (detailElements) => {
             mainContent += '<ul class="list-group">';
             element.comments.forEach(comment => {
                 mainContent += '<li class="list-group-item py-2">';
-                mainContent += '<div><small>Autor: ' + comment.author + ' | Datum: ' + comment.date + '</small></div>'
+                let author = typeof comment.author !== "undefined" ? "Author: " + comment.author + " | " : "";
+                let date = typeof comment.date !== "undefined" ? "Datum: " + luxon.DateTime.fromISO(comment.date).toFormat('dd.MM.yyyy HH:mm:ss ttt') + " | " : "";
+                let id = typeof comment.id !== "undefined" ? "Id: " + comment.id + " | " : "";
+                let initials = typeof comment.initials !== "undefined" ? "Initiale: " + comment.initials + " | " : "";
+                mainContent += (author + date + id + initials).slice(-2) === "| " ? "<div class='text-muted'><small>" + (author + date + id + initials).slice(0, -2) + "</small></div>" : ""; 
                 mainContent += '<div><small>' + comment.commentText + '</small></div>';
                 mainContent += '</li>';
             });
@@ -56,4 +60,3 @@ renderSearchResultDetails = (detailElements) => {
     });
     return mainContent;
 }
-//escapeMarkup(content)
