d1ba3d275a4d2bb1c503b8bf0187a353f58f7bcd
Laszlo Lueck <llueck@gmail.com>
2021-08-26 20:06:56 +0200

    reduce complexity

--- DocSearchAIO/wwwroot/javascript/pagination.js
+++ DocSearchAIO/wwwroot/javascript/pagination.js
@@ -1,15 +1,8 @@
 let getModResult = (documentCount, pageSize) => (documentCount % pageSize === 0) ? 0 : 1;
 let getPagingCount = (documentCount, pageSize) => documentCount <= pageSize ? 0 : (documentCount - documentCount % pageSize) / pageSize + getModResult(documentCount, pageSize);
 
-
-let renderPagination = (documentCount, pageSize, currentPage, searchPhrase) => {
-
-    let pagingCount = getPagingCount(documentCount, pageSize);
-
-    let currentPageNumber = currentPage / pageSize + 1;
-
-    let mainContent = '<nav aria-label="Main-Navigation">';
-    mainContent += '<ul class="pagination">';
+let renderLeftPart = (currentPageNumber, pagingCount, searchPhrase, pageSize, documentCount) => {
+    let mainContent = '';
     if (currentPageNumber > 1 && pagingCount > 1) {
         let link = 'javascript:doSearch(\'' + searchPhrase + '\', ' + (currentPageNumber - 2) * pageSize + ')';
         mainContent += '<li class="page-item">';
@@ -30,6 +23,39 @@ let renderPagination = (documentCount, pageSize, currentPage, searchPhrase) => {
         mainContent += '</div>';
         mainContent += '</li>';
     }
+    return mainContent;
+}
+
+let renderRightPart = (currentPageNumber, pagingCount, searchPhrase, pageSize) => {
+    let mainContent = '';
+    if (currentPageNumber < pagingCount) {
+        let link = 'javascript:doSearch(\'' + searchPhrase + '\', ' + currentPageNumber * pageSize + ')';
+        mainContent += '<li class="page-item">';
+        mainContent += '<a class="page-link" href="' + link + '" aria-label="Vor">';
+        mainContent += '<span aria-hidden="true">&raquo;</span><span class="sr-only">Vor</span>';
+        mainContent += '</a>';
+        mainContent += '</li>';
+    } else {
+        mainContent += '<li class="page-item disabled">';
+        mainContent += '<div class="page-link" tabindex="-1">';
+        mainContent += '<span aria-hidden="true">&raquo;</span><span class="sr-only">Vor</span>';
+        mainContent += '</div>';
+        mainContent += '</li>';
+    }
+    
+    return mainContent;
+}
+
+let renderPagination = (documentCount, pageSize, currentPage, searchPhrase) => {
+
+    let pagingCount = getPagingCount(documentCount, pageSize);
+    let currentPageNumber = currentPage / pageSize + 1;
+
+    let mainContent = '<nav aria-label="Main-Navigation">';
+    mainContent += '<ul class="pagination">';
+
+    mainContent += renderLeftPart(currentPageNumber, pagingCount, searchPhrase, pageSize, documentCount);
+    
     for (let x = 1; x <= pagingCount; x++) {
         let pager = '';
         if ((x - 1) * pageSize === currentPage) {
@@ -68,20 +94,7 @@ let renderPagination = (documentCount, pageSize, currentPage, searchPhrase) => {
         }
     }
 
-    if (currentPageNumber < pagingCount) {
-        let link = 'javascript:doSearch(\'' + searchPhrase + '\', ' + currentPageNumber * pageSize + ')';
-        mainContent += '<li class="page-item">';
-        mainContent += '<a class="page-link" href="' + link + '" aria-label="Vor">';
-        mainContent += '<span aria-hidden="true">&raquo;</span><span class="sr-only">Vor</span>';
-        mainContent += '</a>';
-        mainContent += '</li>';
-    } else {
-        mainContent += '<li class="page-item disabled">';
-        mainContent += '<div class="page-link" tabindex="-1">';
-        mainContent += '<span aria-hidden="true">&raquo;</span><span class="sr-only">Vor</span>';
-        mainContent += '</div>';
-        mainContent += '</li>';
-    }
+    mainContent += renderRightPart(currentPageNumber, pagingCount, searchPhrase, pageSize);
 
     mainContent += '</ul>';
     mainContent += '</nav>';
